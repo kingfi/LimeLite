ebcf4de299ed1e73c9f1410f26af9f72db3438fd
Fianko Buckle <kingfi@fb.com>
2020-07-13 15:36:09 -0700

    Implemented basic bottom nav functionality and created root fragments

--- app/src/main/java/com/example/limelite/MainActivity.java
+++ app/src/main/java/com/example/limelite/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.limelite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.limelite.fragments.MapFragment;
+import com.example.limelite.fragments.NFCFragment;
+import com.example.limelite.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.action_nfc:
+                        fragment = new NFCFragment();
+                        break;
+                    case R.id.action_map:
+                        fragment = new MapFragment();
+                    default:
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.frameLayoutContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_profile);
     }
 }
